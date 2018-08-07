18c5a02009384300ccbec74aec04a9e70a5dcadb
sutheeshsham7 <42170136+sutheeshsham7@users.noreply.github.com>
2018-08-07 14:56:23 +0530

    Create sham1.java

---
+++ sham1.java
@@ -0,0 +1,20 @@
+import java.utill.*;
+class add
+{
+public static viod main(String atgr[])
+{
+int a;
+Scanner a=new.Scanner(System.out.println)
+{
+if(a>0)
+{
+System.out.println("the number positive")
+}
+else if(a<0)
+{
+System.out.println ("the number is negative")
+}
+else
+{
+System.out.println("invalid")
+}
